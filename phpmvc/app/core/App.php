a1715cc66aec14a13d9d849221fef0da4f158373
MRafliansyah <203040105@mail.unpas.ac.id>
2022-10-10 07:56:21 +0700

    assets

--- phpmvc/app/core/App.php
+++ phpmvc/app/core/App.php
@@ -1,50 +1,53 @@
 <?php
 
-class App {
-    protected $controller = 'Home';
-    protected $method = 'index';
-    protected $params = [];
+class App
+{
 
-    public function __construct()
-    {
-        $url = $this->parseURL();
-        // controller
-        if( file_exists('../app/controllers/' . $url[0] . '.php') ) {
-            $this->controller = $url[0];
-            unset($url[0]);
-        }
+  protected $controller = 'Home';
+  protected $method = 'index';
+  protected $params = [];
 
-        require_once '../app/controllers/' .$this->controller . '.php';
-        $this->controller = new $this->controller;
-    
+  public function __construct()
+  {
+    $url = $this->parseURL();
 
-        // method
-        if( isset($url[1]) ){
-            if ( method-exists($this->controller, $url[1]) ) {
-                $this->method = $url[1];
-                unset($url[1]);
-            }
-        }
-
-        // params
-        if( !empty($url) ){
-            $this->params = array_values($url);
-           
-        }
-
-        // jalankan controller & method, serta kirimkan params jika ada
-        call_user_func_array([$this->controller,$this->method], $this->params);
-
+    //controllers
+    if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+      $this->controller = $url[0];
+      unset($url[0]);
     }
 
+    require_once '../app/controllers/' . $this->controller . '.php';
+    $this->controller = new $this->controller;
+
+    //method
+
+    if (isset($url[1])) {
+      if (method_exists($this->controller, $url[1])) {
+        $this->method = $url[1];
+        unset($url[1]);
+      }
+    }
+
+    // parameter
+
+    if (!empty($url)) {
+      $this->params = array_values($url);
+    }
+
+    // jalanakan controller dan menthod, serta kirimkan 
+    // params jika ada
+
+    call_user_func_array([$this->controller, $this->method], $this->params);
+  }
+
+  public function parseURL()
+  {
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
+  }
 }
-
-    public function parseURL()
-    {
-        if( isset($_GET['url']) ) {
-            $url = rtrim($_GET['url'], '/');
-            $url = filter_var($url, FILTER_SANITIZE_URL);
-            $url = explode('/', $url);
-            return $url;
-        }
-    }
